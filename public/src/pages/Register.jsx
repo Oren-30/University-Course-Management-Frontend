2bafeaf6e6ac1ca78ce327909f43a8954b23a67b
sirkiruthi <orencaroli@gmail.com>
2026-07-22 12:07:01 +0300

    Feat add chnages to Register.jsx

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -3,3 +3,15 @@ import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import api from "../services/api";
+
+function Register() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    role: "student",
+  });
