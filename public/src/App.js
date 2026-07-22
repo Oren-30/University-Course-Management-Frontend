5456a373fe13391eab44d68a68d74e2ea52ff000
sirkiruthi <orencaroli@gmail.com>
2026-07-22 11:11:19 +0300

    Feat add changes to app.js

---
+++ public/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Students from "./pages/Students";
+import Courses from "./pages/Courses";
+import Instructors from "./pages/Instructors";
+import Enrollments from "./pages/Enrollments";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
