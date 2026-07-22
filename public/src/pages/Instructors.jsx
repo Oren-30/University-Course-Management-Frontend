65fccf9d3e7018f27dc3257e16e8f0434e0ae70c
sirkiruthi <orencaroli@gmail.com>
2026-07-22 15:02:30 +0300

    Feat add Instructors.jsx

---
+++ public/src/pages/Instructors.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import api from "../services/api";
