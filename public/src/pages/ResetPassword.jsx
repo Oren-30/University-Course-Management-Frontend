29bb4307ae9d7655be573c147601d2b65cc6db9e
sirkiruthi <orencaroli@gmail.com>
2026-07-22 12:28:49 +0300

    Feat add changes to ResetPassword.jsx

---
+++ public/src/pages/ResetPassword.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import api from "../services/api";
