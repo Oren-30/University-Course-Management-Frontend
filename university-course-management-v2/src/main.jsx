2529b2daef0671f1553598f4023c10f9c1007f1f
sirkiruthi <orencaroli@gmail.com>
2026-07-29 09:58:51 +0300

    Feat add chnages to main.jsx

--- university-course-management-v2/src/main.jsx
+++ university-course-management-v2/src/main.jsx
@@ -6,3 +6,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import App from "./App";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
