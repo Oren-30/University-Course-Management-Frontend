e3bcc7896dc5008f39eb2f52a3965ca64a06321c
sirkiruthi <orencaroli@gmail.com>
2026-07-29 10:54:49 +0300

    Feat add changes to Navbar.jsx

--- public/src/components/Navbar.jsx
+++ public/src/components/Navbar.jsx
@@ -11,3 +11,134 @@ function Navbar() {
 
     navigate("/login");
   };
+const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    navigate("/login");
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow">
+
+      <div className="container">
+
+        {/* Logo */}
+        <Link className="navbar-brand fw-bold" to="/">
+          University Course Management System
+        </Link>
+
+
+        {/* Mobile Button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarMenu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+
+        {/* Navigation Links */}
+        <div
+          className="collapse navbar-collapse"
+          id="navbarMenu"
+        >
+
+          <ul className="navbar-nav ms-auto">
+
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+
+
+            {user && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/dashboard">
+                    Dashboard
+                  </Link>
+                </li>
+
+
+                <li className="nav-item">
+                  <Link className="nav-link" to="/students">
+                    Students
+                  </Link>
+                </li>
+
+
+                <li className="nav-item">
+                  <Link className="nav-link" to="/courses">
+                    Courses
+                  </Link>
+                </li>
+
+
+                <li className="nav-item">
+                  <Link className="nav-link" to="/instructors">
+                    Instructors
+                  </Link>
+                </li>
+
+
+                <li className="nav-item">
+                  <Link className="nav-link" to="/enrollment">
+                    Enrollments
+                  </Link>
+                </li>
+
+
+                <li className="nav-item">
+                  <Link className="nav-link" to="/profile">
+                    Profile
+                  </Link>
+                </li>
+              </>
+            )}
+
+
+            {!user ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+
+
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                </li>
+              </>
+            ) : (
+
+              <li className="nav-item">
+                <button
+                  className="btn btn-light ms-2"
+                  onClick={logout}
+                >
+                  Logout
+                </button>
+              </li>
+
+            )}
+
+
+          </ul>
+
+        </div>
+
+      </div>
+
+    </nav>
+  );
+}
+
+export default Navbar;
