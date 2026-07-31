5f821ea447194f015d26ca7553951059517e7426
sirkiruthi <orencaroli@gmail.com>
2026-07-31 03:48:35 +0300

    Feat add changes to Sidebar.jsx

--- university-course-management/src/components/Sidebar.jsx
+++ university-course-management/src/components/Sidebar.jsx
@@ -3,69 +3,82 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
 
-  return (
-    <div className="p-3">
+  const user = JSON.parse(localStorage.getItem("user"));
 
-      <h5 className="mb-4">
-        CMS University
-      </h5>
+  return (
+
+    <div className="bg-light border-end vh-100 p-3">
+
+      <h4 className="mb-4">
+        Menu
+      </h4>
 
       <ul className="nav flex-column">
 
-        <li className="nav-item">
-          <Link 
-            className="nav-link"
+        <li className="nav-item mb-2">
+          <Link
             to="/dashboard"
+            className="nav-link"
           >
             Dashboard
           </Link>
         </li>
 
+        {(user?.role === "admin" ||
+          user?.role === "instructor") && (
 
-        <li className="nav-item">
-          <Link 
-            className="nav-link"
-            to="/students"
-          >
-            Students
-          </Link>
-        </li>
+          <li className="nav-item mb-2">
+            <Link
+              to="/students"
+              className="nav-link"
+            >
+              Students
+            </Link>
+          </li>
 
+        )}
 
-        <li className="nav-item">
-          <Link 
-            className="nav-link"
+        <li className="nav-item mb-2">
+          <Link
             to="/courses"
+            className="nav-link"
           >
             Courses
           </Link>
         </li>
 
-
-        <li className="nav-item">
-          <Link 
-            className="nav-link"
+        <li className="nav-item mb-2">
+          <Link
             to="/instructors"
+            className="nav-link"
           >
             Instructors
           </Link>
         </li>
 
-
-        <li className="nav-item">
-          <Link 
-            className="nav-link"
+        <li className="nav-item mb-2">
+          <Link
             to="/enrollments"
+            className="nav-link"
           >
             Enrollments
+          </Link>
+        </li>
+
+        <li className="nav-item mb-2">
+          <Link
+            to="/profile"
+            className="nav-link"
+          >
+            My Profile
           </Link>
         </li>
 
       </ul>
 
     </div>
+
   );
 }
-
 
 export default Sidebar;
