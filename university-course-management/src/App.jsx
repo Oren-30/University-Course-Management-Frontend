63739fa25eb881bff914ed128096d39b970149dd
sirkiruthi <orencaroli@gmail.com>
2026-07-31 03:24:09 +0300

    Feat add changes to App.jsx

--- university-course-management/src/App.jsx
+++ university-course-management/src/App.jsx
@@ -1,133 +1,177 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Pages
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
 import Dashboard from "./pages/Dashboard";
 import Students from "./pages/Students";
 import Courses from "./pages/Courses";
 import Instructors from "./pages/Instructors";
 import Enrollments from "./pages/Enrollments";
 import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
-// Components
 import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
 
-  return (
+    return (
 
-    <BrowserRouter>
+        <BrowserRouter>
 
-      <Routes>
+            <Routes>
 
-        {/* Public Routes */}
+                {/* Public Routes */}
 
-        <Route 
-          path="/" 
-          element={<Home />} 
-        />
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
 
-        <Route 
-          path="/login" 
-          element={<Login />} 
-        />
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
 
-        <Route 
-          path="/register" 
-          element={<Register />} 
-        />
+                <Route
+                    path="/register"
+                    element={<Register />}
+                />
 
-        <Route 
-          path="/forgot-password" 
-          element={<ForgotPassword />} 
-        />
+                <Route
+                    path="/about"
+                    element={<About />}
+                />
 
-        <Route 
-          path="/reset-password" 
-          element={<ResetPassword />} 
-        />
-
-
-        {/* Protected Routes */}
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+                <Route
+                    path="/contact"
+                    element={<Contact />}
+                />
 
 
-        <Route
-          path="/students"
-          element={
-            <ProtectedRoute>
-              <Students />
-            </ProtectedRoute>
-          }
-        />
+
+                {/* Dashboard */}
+
+                <Route
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <Dashboard />
+                        </ProtectedRoute>
+                    }
+                />
 
 
-        <Route
-          path="/courses"
-          element={
-            <ProtectedRoute>
-              <Courses />
-            </ProtectedRoute>
-          }
-        />
+
+                {/* Students */}
+
+                <Route
+                    path="/students"
+                    element={
+                        <ProtectedRoute
+                            roles={[
+                                "admin",
+                                "instructor"
+                            ]}
+                        >
+                            <Students />
+                        </ProtectedRoute>
+                    }
+                />
 
 
-        <Route
-          path="/instructors"
-          element={
-            <ProtectedRoute>
-              <Instructors />
-            </ProtectedRoute>
-          }
-        />
+
+                {/* Courses */}
+
+                <Route
+                    path="/courses"
+                    element={
+                        <ProtectedRoute
+                            roles={[
+                                "admin",
+                                "instructor",
+                                "student"
+                            ]}
+                        >
+                            <Courses />
+                        </ProtectedRoute>
+                    }
+                />
 
 
-        <Route
-          path="/enrollments"
-          element={
-            <ProtectedRoute>
-              <Enrollments />
-            </ProtectedRoute>
-          }
-        />
+
+                {/* Instructors */}
+
+                <Route
+                    path="/instructors"
+                    element={
+                        <ProtectedRoute
+                            roles={[
+                                "admin",
+                                "instructor",
+                                "student"
+                            ]}
+                        >
+                            <Instructors />
+                        </ProtectedRoute>
+                    }
+                />
 
 
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+
+                {/* Enrollments */}
+
+                <Route
+                    path="/enrollments"
+                    element={
+                        <ProtectedRoute
+                            roles={[
+                                "admin",
+                                "instructor",
+                                "student"
+                            ]}
+                        >
+                            <Enrollments />
+                        </ProtectedRoute>
+                    }
+                />
 
 
-        {/* Default route */}
 
-        <Route
-          path="*"
-          element={<Home />}
-        />
+                {/* Profile */}
 
-      </Routes>
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <Profile />
+                        </ProtectedRoute>
+                    }
+                />
 
-    </BrowserRouter>
 
-  );
+
+                {/* Catch All */}
+
+                <Route
+                    path="*"
+                    element={<NotFound />}
+                />
+
+            </Routes>
+
+        </BrowserRouter>
+
+    );
 
 }
 
